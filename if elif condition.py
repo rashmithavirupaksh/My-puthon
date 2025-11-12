afda7b618dd5e88a000a0daa1095c98c9ac44af7
rashmithavirupaksh <rashmithabatti@gmail.com>
2025-11-12 11:27:26 +0530

    Create if elif condition.py

---
+++ if elif condition.py
@@ -0,0 +1,8 @@
+# if elif condition
+a=int(input("enter the number"))
+if a>90:
+  print("the number is positive")
+elif a<60:
+  print("the number is negative")
+else:
+  print("the number is zero")

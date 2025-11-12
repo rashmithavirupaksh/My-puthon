7d148be3655164961b7622ecb0c1c8677346b824
rashmithavirupaksh <rashmithabatti@gmail.com>
2025-11-12 11:30:46 +0530

    Create largest three numbers.py

---
+++ largest three numbers.py
@@ -0,0 +1,10 @@
+# largest three numbers
+num1=int(input("enter the first number"))
+num2=int(input("enter the second number"))
+num3=int(input("enter the third number"))
+if num1<num2:
+  print(num2,"the largest number is")
+elif num2<num3:
+  print(num3,"the largest number is")
+else:
+  print(num1,"the largest number is")

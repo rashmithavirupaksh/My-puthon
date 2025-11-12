3d630407431a6d336eb311cf2fcdb829d7da4e47
rashmithavirupaksh <rashmithabatti@gmail.com>
2025-11-12 22:31:12 +0530

    Create Largest_of_two.py

---
+++ Largest_of_two.py
@@ -0,0 +1,8 @@
+def largest_of_two(a, b):
+    if a > b:
+        print(f"{a} is larger.")
+    elif b > a:
+        print(f"{b} is larger.")
+    else:
+        print("Both numbers are equal.")
+largest_of_two(8, 25)

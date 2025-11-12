fa29618de3a9066b51f184dbaee2f2b76d1f5ded
rashmithavirupaksh <rashmithabatti@gmail.com>
2025-11-12 11:28:53 +0530

    Create positive negative number.py

---
+++ positive negative number.py
@@ -0,0 +1,8 @@
+# if number is positive or negative or zero
+a=float(input("enter the number"))
+if a>0:
+  print("positive number")
+if a<0:
+  print("negative number")
+else:
+  print("it is zero")

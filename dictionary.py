100e85ab69d7b38f30b6aaec7b49fd8f61b67cdd
rashmithavirupaksh <rashmithabatti@gmail.com>
2025-11-14 11:31:27 +0530

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,13 @@
+batti={
+    "name":"rashmitha",
+    "age":17,
+    "college":"sridevi",
+
+}
+print(batti)
+batti["date"]=5
+print(batti)
+del batti["age"]
+print(batti)
+for i in a:
+  print(i,batti)
